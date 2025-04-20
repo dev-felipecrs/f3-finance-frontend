5aa66859da95b7e2192aa03f61fa1ba5c9bc9f06
Felipe Costa <felipecrs32@gmail.com>
2025-04-20 14:02:45 -0300

    feat: add transition in header list

--- src/presentation/components/shared/Header/index.tsx
+++ src/presentation/components/shared/Header/index.tsx
@@ -8,7 +8,7 @@ export function Header() {
     <header className="flex items-center justify-end bg-white p-6">
       <Link
         href="#"
-        className="flex h-16 w-16 items-center justify-center rounded-lg border border-gray-200 bg-white p-[1.125rem] hover:brightness-95"
+        className="flex h-16 w-16 items-center justify-center rounded-lg border border-gray-200 bg-white p-[1.125rem] transition-all hover:brightness-95"
       >
         <User size={24} />
       </Link>
