ebd13b0a2a59c55af45e3960898eb81494150ba8
Guilherme Marques <guilhermehenrique12308@gmail.com>
2025-04-23 16:41:21 -0300

    feat: add create password page and form component

---
+++ src/app/accounts/create-password/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { Logo } from '@/presentation/components/shared'
+import { Form } from '@/presentation/components/pages/accounts/create-password'
+
+type CreatePasswordPageParams = {
+  searchParams: Promise<{
+    auth: string
+  }>
+}
+
+export default async function CreatePasswordPage({
+  searchParams,
+}: CreatePasswordPageParams) {
+  const { auth } = await searchParams
+
+  return (
+    <>
+      <header className="flex items-center justify-center">
+        <Logo />
+      </header>
+
+      <div className="mt-10 flex flex-col gap-5">
+        <h1 className="text-4xl leading-14 font-bold">Crie sua senha</h1>
+        <p className="text-lg text-gray-400">
+          Quase lá! Crie uma senha segura e finalize seu acesso ao sistema
+        </p>
+      </div>
+
+      <Form authToken={auth} />
+    </>
+  )
+}
