8e0ea284c6cd9b4b0d911a8a3df0a436c7ded58c
Guilherme Marques <guilhermehenrique12308@gmail.com>
2025-09-18 12:02:12 -0300

    feat: add make delete user

--- src/domain/use-cases/users/index.ts
+++ src/domain/use-cases/users/index.ts
@@ -1,3 +1,4 @@
 export * from './i-find-all-users'
 export * from './i-get-user-by-email'
 export * from './i-invite-user'
+export * from './i-delete-user'
--- src/infra/factories/users/index.ts
+++ src/infra/factories/users/index.ts
@@ -1,3 +1,4 @@
 export * from './make-find-all-users-use-case'
 export * from './make-get-user-by-email-use-case'
 export * from './make-invite-user-use-case'
+export * from './make-delete-user'
